e978ae03d446384f3543c2e44c1c08ca5a3f4007
auroradream04 <alvs131313@gmail.com>
2024-03-11 19:29:02 +0800

    Added node modules back on .gitignore and finished prototype v1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-// Text Replacer 
+import fs from 'fs';
 
-console.log('Hello world!')
-console.log('Hello world!')
+const keys = {
+    "Hello": "Hi",
+    "Up": "Down",
+    "Big": "Small",
+};
+
+const article = fs.readFileSync('./articles/1.txt', 'utf-8');
+
+function replaceKeysWithValues(text:any, keys:any) {
+    let result = text;
+    for (const [key, value] of Object.entries(keys)) {
+        const regex = new RegExp(key, 'gi');
+        result = result.replace(regex, value);
+    }
+    return result;
+}
+
+const modifiedArticle = replaceKeysWithValues(article, keys);
+
+fs.writeFileSync('./out/1.txt', modifiedArticle);
